8db182128c98e640774476d4d59e09f673819c56
Luke Jackson <lj00021@mix.wvu.edu>
2026-03-24 11:08:34 -0400

    Finishing First Row Count Table with all tables from dataset unioned into a single table

--- data_quality.sql
+++ data_quality.sql
@@ -1,4 +1,20 @@
 ##Quality Audit
 #Counting rows in each Table by selecting each table and counting every row using count(*)
-SELECT 'customers' AS tableName, COUNT(*) As rowCount FROM customers
 
+SELECT 'categoryTranslation' AS tableName, COUNT(*) As rowCount FROM category_translation
+    UNION ALL
+SELECT 'customers', COUNT(*) FROM customers
+    UNION ALL
+SELECT 'geolocation', COUNT(*) FROM geolocation
+    UNION ALL
+SELECT 'orderItems', COUNT(*) FROM order_items
+    UNION ALL
+SELECT 'orderPayments', COUNT(*) FROM order_payments
+    UNION ALL
+SELECT 'orderReviews', COUNT(*) FROM order_reviews
+    UNION ALL
+SELECT 'orders', COUNT(*) FROM orders
+    UNION ALL
+SELECT 'products', COUNT(*) FROM products
+    UNION ALL
+SELECT 'sellers', COUNT(*) FROM sellers
