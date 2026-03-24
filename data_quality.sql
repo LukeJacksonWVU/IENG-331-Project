db84291dd97830d0ebcb9732fa389af2b067a36c
Luke Jackson <lj00021@mix.wvu.edu>
2026-03-24 11:02:40 -0400

    First Query to test, starting with the first table in the dataset, will add rest after this works

--- data_quality.sql
+++ data_quality.sql
@@ -1,1 +1,4 @@
-#Olist Quality Audit
+##Quality Audit
+#Counting rows in each Table by selecting each table and counting every row using count(*)
+SELECT 'customers' AS tableName, COUNT(*) As rowCount FROM customers
+
