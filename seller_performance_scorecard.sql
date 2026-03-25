6284cc523ced4e8cfb92ee4340ebd5c34aef4640
Gavin <grm00010@mix.wvu.edu>
2026-03-24 21:00:24 -0400

    sets up for one time and not on time deliveries and gets rid of nulls

--- seller_performance_scorecard.sql
+++ seller_performance_scorecard.sql
@@ -9,3 +9,25 @@ WITH seller_revenue AS (
     GROUP BY oi.seller_id
 ),
 --seller ids in items; sum price and freight to get total cost and group by seller (cancellations ignored)
+seller_delivery AS (
+    SELECT
+        oi.seller_id,
+        ROUND(
+            SUM(
+                CASE
+                    WHEN CAST(o.order_delivered_customer_date AS DATE)
+                         <= CAST(o.order_estimated_delivery_date AS DATE)
+                    THEN 1 ELSE 0
+                END
+                --if it was delivered on time (1) if it was not (0)
+            ) * 100.0 / COUNT(DISTINCT o.order_id),
+        2) AS on_time_rate_pct
+        --converts to  rate of on time
+    FROM olist.main.order_items oi
+    JOIN olist.main.orders o ON oi.order_id = o.order_id
+    WHERE o.order_status = 'delivered'
+      AND o.order_delivered_customer_date IS NOT NULL
+      AND o.order_estimated_delivery_date IS NOT NULL
+    GROUP BY oi.seller_id
+),
+--gets rid of null
