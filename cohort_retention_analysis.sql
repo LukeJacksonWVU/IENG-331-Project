9a57119dce51ac2c5298b8036254020c0cf4ec4a
Gavin <grm00010@mix.wvu.edu>
2026-03-24 20:24:31 -0400

    join order and customer, make unique customers by using geolocation

--- cohort_retention_analysis.sql
+++ cohort_retention_analysis.sql
@@ -1,1 +1,11 @@
-##test
+WITH resolved_customers AS (
+    SELECT
+        o.order_id,
+        o.order_purchase_timestamp,
+        c.customer_zip_code_prefix || '_' || c.customer_city    AS customer_unique
+    FROM olist.main.orders o
+    JOIN olist.main.customers c
+        ON o.customer_id = c.customer_id
+    WHERE o.order_status = 'delivered'
+),
+--join order and customers with location attached, eliminates duplicate id's by using location (customer_unique)
